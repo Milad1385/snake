93db86e3c2079ee9e0be7eedf47c10953a60c18d
milad salamian <miladsalami1385@gmail.com>
2023-09-21 06:09:48 -0700

    fix snake bug

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 const scoreElem = document.querySelector(".score");
-console.log(statusElem);
 let scale = 10;
 let rows = canvas.width / scale; //=> 40
 let columns = canvas.height / scale; //=> 40
